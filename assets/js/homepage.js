e0fecea6c638c0d902bfc9d9941d9fd0169c5867
michmash2010 <michellecrowe2010@gmail.com>
2021-09-26 21:45:41 -0400

    add time comparison and coloring to be used later when dynamically creating time blocks

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,33 @@
 var dateEl = document.querySelector("#currentDay");
+var time = document.querySelector(".time-block").innerText.replace("AM", ":00am").replace("PM", ":00pm");
+var descriptionEl = document.querySelector(".col-10");
 
-dateEl.textContent = moment().format("MMMM D, YYYY");
+console.log(time);
 
+dateEl.textContent = moment().format("dddd, MMMM D, YYYY");
+
+// var determineRelativeTime = function() {
+//     var currentTime = moment().format('h');
+    
+//     console.log("Hour of Current time is: " + currentTime);
+ 
+// };
+
+console.log(moment().format('h'));
+
+var auditTask = function(time) {
+    if (moment().isAfter(time), 'hour') {
+        $(descriptionEl).addClass("past");
+        console.log("Past, moment is after time");
+    }
+    else if (moment().isBefore(time), 'hour') {
+        console.log("Future, moment is before time");
+    }
+    else {
+        console.log("Present");
+    }
+};
+
+//determineRelativeTime();
+
+auditTask();
